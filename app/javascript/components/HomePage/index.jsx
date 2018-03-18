ad5332a3f8ad43ae460d77ae7da76acd55f1639f
locovna <locovna@gmail.com>
2018-03-18 21:32:53 +0200

    Create basic landing page

--- app/javascript/components/HomePage/index.jsx
+++ app/javascript/components/HomePage/index.jsx
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM  from 'react-dom';
 import PropTypes from 'prop-types';
 
-export default class MainPage extends Component {
+export default class HomePage extends Component {
   render() {
     return(
-      <div>
-        Welcome to the Palace.
+      <div className='home_page'>
+        <div className='logo'><img src='/assets/knife.svg'/></div>
+        <h1>Welcome to the Palace.</h1>
+        <div className='home_page_content'>
+          <p>You are in Library 2M online library. We are very happy to present renewed Fiction Library 2.7M online library and 58M science  magazine hub.</p>
+          <button>Explore</button>
+          <button>Sign Up</button>
+        </div>
       </div>
     )
   }
--- app/javascript/packs/home_page.jsx
+++ app/javascript/packs/home_page.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React     from 'react'
+import ReactDOM  from 'react-dom'
 import PropTypes from 'prop-types'
-
-import HomePage from 'components/HomePage'
+import HomePage  from 'components/HomePage'
 
 document.addEventListener('DOMContentLoaded', () => {
   const appContainer = document.getElementsByClassName('app-container')[0];
